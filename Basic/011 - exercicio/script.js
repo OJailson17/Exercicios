27125a7eb024f1bed2baa955defc036f3b21b9bd
OJaylson17 <JaylsonO17@gmail.com>
2020-09-02 19:40:46 -0300

    Exercicio que retorna o número de palavras de uma string

---
+++ Basic/011 - exercicio/script.js
@@ -0,0 +1,8 @@
+//Escreva um  programa que dado uma sentença, retorne o numero de palavras contidas nessa sentença.
+
+function sentence(str){
+    let numWord = str.split(' ');
+    return numWord.length;
+}
+
+console.log(sentence('hello world!, olá mundo'));
