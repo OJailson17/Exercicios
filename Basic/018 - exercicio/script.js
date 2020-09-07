86efe96b618301af59e98e2fdf06fb9ae015d951
OJaylson17 <JaylsonO17@gmail.com>
2020-09-06 22:15:16 -0300

    retorna um array de strings em ordem alfabética

---
+++ Basic/018 - exercicio/script.js
@@ -0,0 +1,7 @@
+// Escreva uma função que recebe um array de strings e retorna todas as suas letras em ordem alfabética.
+
+function abc(str){
+    return str.sort();
+}
+
+console.log(abc(['hello', 'world', 'hi', 'welcome']));
