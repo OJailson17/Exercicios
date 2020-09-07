ebff48e5c0bee8c3bb45488b19e9ab128f1e0557
OJaylson17 <JaylsonO17@gmail.com>
2020-09-06 21:59:50 -0300

    exercicio que rotorna o maior numéro

---
+++ Basic/017 - exercicio/script.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program that accept two integers and display the larger
+
+function greater(num1, num2){
+    if(num1 > num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+
+//console.log(greater(15, 5));
